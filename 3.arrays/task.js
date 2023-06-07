e9da08f7c73ddfcabb731e297457037a3c82eea7
Paul White <paratrak@list.ru>
2023-06-07 12:41:56 +0300

    Третье дз готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+    if (
+        arr1.length === arr2.length &&
+        arr1.every((element, index) => element === arr2[index])
+      ) {
+        return true;
+      }
+      return false;
+
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users
+		.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((sum, age, index, array) => sum + age / array.length, 0);
+	return result;
+
 }
